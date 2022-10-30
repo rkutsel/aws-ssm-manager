efc4d6284b80c525e546d4e57c897d6b2c340b92
rkutsel <roman.kutsel@docusign.com>
2022-10-29 23:05:13 -0700

    save-to-file bugfix

--- sdk-ssm.js
+++ sdk-ssm.js
@@ -35,6 +35,7 @@ export function createSecret(secret, value, profile, region, tags = null) {
 export function getSecrets(type, format, profile, region) {
   process.env.AWS_PROFILE = profile;
   const fmt = ssmConfigOptions.format;
+  let jsonObj = [];
 
   const ssm = new AWS.SSM({ region: region });
   const params = {
@@ -50,35 +51,45 @@ export function getSecrets(type, format, profile, region) {
 
   function _getRecursive() {
     ssm.describeParameters(params, (err, data) => {
+      const paramsData = data.Parameters;
       if (err) {
         console.log(
           `RUNTIME ERROR!\n MESSAGE: ${err.message}\n, CODE: ${err.code}\n, TIME: ${err.time}\n`
         );
       } else {
-        if (format === fmt.json) {
-          console.log(data.Parameters);
+        switch (format) {
+          case fmt.json:
+            console.log(data.Parameters);
+            break;
+
+          case fmt.text:
+            data.Parameters.map((el) =>
+              console.log(
+                `Secret Name: ${el.Name}\nLast Modified: ${
+                  el.LastModifiedDate
+                }\nLast Modified By: ${
+                  el.LastModifiedUser.split("/")[
+                    el.LastModifiedUser.split("/").length - 1
+                  ]
+                }\n`
+              )
+            );
+            break;
+
+          case fmt.file:
+            !data.NextToken
+              ? paramsData.map((el) => {
+                  jsonObj.push(el);
+                }) && saveToFile(jsonObj, profile, region)
+              : paramsData.map((el) => {
+                  jsonObj.push(el);
+                });
+            break;
         }
-        if (format === fmt.text) {
-          data.Parameters.map((el) =>
-            console.log(
-              `Secret Name: ${el.Name}\nLast Modified: ${
-                el.LastModifiedDate
-              }\nLast Modified By: ${
-                el.LastModifiedUser.split("/")[
-                  el.LastModifiedUser.split("/").length - 1
-                ]
-              }\n`
-            )
-          );
-        }
-        if (format === fmt.file) {
-          const params = data.Parameters;
-          // const filename = `${profile}-${region}`
-          saveToFile(params, profile, region);
-        }
+
         if (data.NextToken) {
           params.NextToken = data.NextToken;
-          _getRecursive(data.Parameters);
+          _getRecursive();
         }
       }
     });
